35a61b6e09a222b33b1b9cf608e6926f036d3ba3
Mert Çakar <mertcakar.es@gmail.com>
2019-07-04 22:57:12 +0300

    ControllerAdvice is added.

---
+++ src/main/java/com/chivalrous/app/ws/exception/AppExceptionshandler.java
@@ -0,0 +1,21 @@
+package com.chivalrous.app.ws.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class AppExceptionshandler {
+
+	@ExceptionHandler(value = { NullPointerException.class })
+	public ResponseEntity<Object> handleAnyException(NullPointerException ex) {
+		return new ResponseEntity<>("Error : " + ex.getMessage(),
+				HttpStatus.BAD_REQUEST);
+	}
+
+	@ExceptionHandler(value = Exception.class)
+	public ResponseEntity<Object> exception(Exception exception) {
+		return new ResponseEntity<>("Error...", HttpStatus.NOT_FOUND);
+	}
+}
